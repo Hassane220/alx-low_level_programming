81fff6289db6cc51d4fedaae38242dfece585ea3
Hassane220 <hassane220soum@gmail.com>
2022-12-08 14:10:19 -0800

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,17 @@
+#include <stdio.h>
+
 /**
  *  main -Print a line of with puts function
  *
- *   Return: 0
- *
-*/
-
-
+ *  Return: 0
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
